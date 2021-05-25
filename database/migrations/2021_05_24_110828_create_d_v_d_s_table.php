c750513bd19e26402d518c6a036d3f37647c1ed1
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2021-05-25 11:27:55 +0700

    Membuat database dan data awal dvd

---
+++ database/migrations/2021_05_24_110828_create_d_v_d_s_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDVDSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dvd', function (Blueprint $table) {
+            $table->String('id_dvd', 15)->primary();
+            $table->String('nama_dvd', 20)->nullable();
+            $table->String('harga_dvd', 10)->nullable();
+            $table->String('status_dvd', 50)->nullable();
+            $table->String('image_dvd', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dvd');
+    }
+}
---
+++ database/seeders/DVDSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DVDSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data=[[
+            'id_dvd' => '1',
+            'nama_dvd' =>'BAD BOYS',
+            'harga_dvd' => '15000',
+            'status_dvd' => 'belum dipinjam',
+            'image_dvd' => '/template/images/BAD BOYS.jpg'
+        ],[
+            'id_dvd' => '2',
+            'nama_dvd' =>'GANGSTER',
+            'harga_dvd' => '15000',
+            'status_dvd' => 'belum dipinjam',
+            'image_dvd' => '/template/images/GANGSTER.jpg'
+        ],[
+            'id_dvd' => '3',
+            'nama_dvd' =>'KOMEDI MODEREN',
+            'harga_dvd' => '15000',
+            'status_dvd' => 'belum dipinjam',
+            'image_dvd' => '/template/images/KOMEDI MODEREN.jpg'
+        ],[
+            'id_dvd' => '4',
+            'nama_dvd' =>'IRON MAN 1',
+            'harga_dvd' => '15000',
+            'status_dvd' => 'belum dipinjam',
+            'image_dvd' => '/template/images/iron man 1.jpg'
+        ],[
+            'id_dvd' => '5',
+            'nama_dvd' =>'IRON MAN 1',
+            'harga_dvd' => '15000',
+            'status_dvd' => 'belum dipinjam',
+            'image_dvd' => '/template/images/iron man 2.jpg'
+        ]    
+        ];
+        DB::table('dvd')->insert($data);
+    }
+}
